d1568f991490e9a8f9cb61c430d6b2cd461466bc
tan05 <tan.tantham.tan@gmail.com>
2024-05-13 23:29:58 +0700

    finished

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from tkinter import *
+import string
+import random
+
+string.ascii_letters
 
 root = Tk()
 root.title("Password generator")
@@ -9,10 +13,26 @@ ArialSmall = ("Arial", 10)
 title = Label(root, text="PASSWORD GENERATOR")
 title.configure(font=Arial)
 
-generateButton = Button(root, text="generate")
+text = Entry(root)
+
+def generatePassword():
+    password = ""
+
+    i = 0
+    
+    while i <= 10:
+        password += random.choice(string.ascii_letters)
+        i += 1
+
+    text.delete(0,"end")
+
+    text.insert(0, password)
+
+generateButton = Button(root, text="generate", command=generatePassword)
 generateButton.configure(font=ArialSmall)
 
 title.pack()
+text.pack()
 generateButton.pack()
 
 root.mainloop()
