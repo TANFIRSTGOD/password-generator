120da9881ae9af08b025c16e22aa45657f238adc
tan05 <tan.tantham.tan@gmail.com>
2024-05-13 23:21:19 +0700

    BUTTON

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from tkinter import *
 
 root = Tk()
+root.title("Password generator")
+
+Arial = ("Arial", 15) 
+ArialSmall = ("Arial", 10) 
 
 title = Label(root, text="PASSWORD GENERATOR")
+title.configure(font=Arial)
+
+generateButton = Button(root, text="generate")
+generateButton.configure(font=ArialSmall)
+
+title.pack()
+generateButton.pack()
 
 root.mainloop()
